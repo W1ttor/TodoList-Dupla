9fd577c7000a3a7b905b4ff3e739272095136511
W1ttor <wittinhosilva42@gmail.com>
2025-12-01 20:58:51 -0300

    Adicionando pastas e modulo Spring

---
+++ ToDolistBack/src/main/java/com/doido/todolistback/ToDolistBackApplication.java
@@ -0,0 +1,13 @@
+package com.doido.todolistback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDolistBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDolistBackApplication.class, args);
+	}
+
+}
---
+++ ToDolistBack/src/test/java/com/doido/todolistback/ToDolistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doido.todolistback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDolistBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
