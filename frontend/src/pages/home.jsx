ce6fb0786e7616e08ad4a10fd265253d028ded3d
david santos <david.silva@bsb.ferreiraechagas.local>
2026-02-13 11:00:58 -0300

    feat: home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-/* =========================
+/* ==========================
    SIDEBAR
 ========================= */
 function Sidebar({
